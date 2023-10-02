76ebdf9440dd26a4443add8ccf3e1ebafb536b7d
spitliner <>
2023-10-02 21:42:42 +0700

    init auth

--- Backend/source/middleware/auth.ts
+++ Backend/source/middleware/auth.ts
@@ -1,31 +1,61 @@
 require("dotenv").config();
 import argon2 = require('argon2');
+import jwt from "jsonwebtoken";
+import { Algorithm } from "jsonwebtoken";
+import crypto from 'crypto';
+//import privateKey from "../config/loadPrivateKey.js";
+//import publicKey from "../config/loadPublickey.js";
 
 class Authentication {
-    public static async passwordHash (password : string) {
+    static async hashPassword(password : string) {
+        return argon2.hash(password, {
+            type: argon2.argon2id,
+            hashLength: 100,
+            timeCost: 4,
+        });
+    }
+
+    static async verifyPassword(password : string, hash : string) {
+        return argon2.verify(hash, password);
+    }
+
+    static createToken(payload : any) {
+        payload["padding"] = crypto.randomBytes(48).toString('base64');
+        return jwt.sign(payload, {key: "privateKey", passphrase: String(process.env["KEY_PASS"]}), {
+            header: {
+                alg: "ES384",
+                typ: "JWT"
+            },
+            algorithm: "ES384",
+            expiresIn: "24h",
+            notBefore: Math.floor(Date.now() / 1000)
+        });
+    }
+
+    static verifyToken(token : string) {
+        let decode = null;
         try {
-            return argon2.hash(password, {
-                type: argon2.argon2id,
-                memoryCost: 2 ** 16,
-                timeCost: 4,
-                parallelism: 6,
-                hashLength: 100
+            decode = jwt.verify(token, "publicKey", {
+                algorithms: ["ES384"],
             });
         } catch (error) {
-            return Promise.reject(error);
-        }
-    }
-
-    public static async validatePassword (hash : string, password : string) {
-        try {
-            if (hash.startsWith("$argon2")) {
-                return argon2.verify(hash, password);
+            if (error instanceof jwt.TokenExpiredError) {
+                return {
+                    "Error": "expired token"
+                }
+            } else if (error instanceof jwt.JsonWebTokenError) {
+                return {
+                    "Error": "invalid token"
+                }
+            } else return {
+                "Error": "server error"
             }
-        } catch (error) {
-            return Promise.reject(error);
         }
-        return Promise.reject();
+        
+        return decode;
     }
 }
+//console.log(AuthMiddle.verifyToken(AuthMiddle.createToken({a: "b"})));
+//console.log(AuthMiddle.createToken({a: "b"}));
 
-module.exports = Authentication;
+export default Authentication;
