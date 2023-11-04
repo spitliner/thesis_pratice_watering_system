3d9a6075835b209de33d491bf8d77ce1f12a975a
catkhanhnguyen123 <hrin2211@gmail.com>
2023-11-04 22:20:35 +0700

    update pages Home, Schedules, Login, Profile

---
+++ src/pages/Home/DropdownMenu.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import Avatar from '@mui/material/Avatar'
+import Typography from '@mui/material/Typography'
+
+function DropdownMenu() {
+  const [anchorEl, setAnchorEl] = React.useState(null)
+  const open = Boolean(anchorEl)
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+
+  }
+  return (
+    <Box>
+      <Button
+        id="basic-button"
+        aria-controls={open ? 'basic-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+        endIcon={<ExpandMoreIcon />}
+      >
+        <Avatar
+          sx={{ width: 32, height: 32 }}
+          src = 'https://i.pinimg.com/736x/57/3f/f1/573ff1a3bea0c77246affaf18bb39b48.jpg'
+        >
+            M</Avatar>
+        <Typography sx={{ fontSize: '1rem', fontWeight: 'bold', px: 1 }}>Kate</Typography>
+      </Button>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button'
+        }}
+      >
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>My account</MenuItem>
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+      </Menu>
+    </Box>
+
+  )
+}
+
+export default DropdownMenu
