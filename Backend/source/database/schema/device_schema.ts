f704ec29429660d4f45c1e4c4fad2052d5794a54
spitliner <>
2023-12-15 17:50:59 +0700

    add ada request

--- Backend/source/database/schema/device_schema.ts
+++ Backend/source/database/schema/device_schema.ts
@@ -1,14 +1,16 @@
 import mongoose from "mongoose";
 
 const DeviceSchema = new mongoose.Schema({
-    id: { type: String, required: true, index: { type: 1, unique: true } },
+    id: { type: String, required: true },
     userID: { type: String, required: true, index: 1 },
     name: { type: String, required: true},
     type: { type: String, required: true },
     apiKey: { type: String, required: true },
     adaUserName: { type: String, required: true, index: 1 },
+    settings: { type: String, required: true },
     schedules: { type: [[String]], required: false },
-    settings: { type: String, required: true } 
 });
+
+DeviceSchema.index({id: 1, userID: 1}, {unique: true});
 
 export default DeviceSchema;
